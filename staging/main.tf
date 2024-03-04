aa4b4d92ed183f6de29f6c79505a20fa98346a17
bibhutimail <bibhutimail@gmail.com>
2024-03-04 07:28:40 +0530

    re-initilize

--- staging/main.tf
+++ staging/main.tf
@@ -1,31 +1,31 @@
 
-# module "ec2_instance" {
-#   source          = "../modules/ec2"
-#   ami_id          = var.ami_id
-#   instance_type   = var.instance_type
-#   subnet_id       = module.vpc_network.sub_pub_id
-#   security_groups = [ module.vpc_network.sg_ssh_allow ]
+module "ec2_instance" {
+  source          = "../modules/ec2"
+  ami_id          = var.ami_id
+  instance_type   = var.instance_type
+  subnet_id       = module.vpc_network.sub_pub_id
+  security_groups = [ module.vpc_network.sg_ssh_allow ]
+  env             = var.env
+  team            = var.team
+}
+
+
+module "vpc_network" {
+  source          = "../modules/vpc"
+  cidr_block      = var.vpc_cidr_block
+  vpc_tag         = var.vpc_tag
+  public_subnet   = var.public_subnet_cidr_block
+  private_subnet  = var.private_subnet_cidr_block
+  env             = var.env
+ }
+
+# module "lambda_create"{
+#   source          = "../modules/lambda"
 #   env             = var.env
-#   team            = var.team
+#   function_name   = var.env #Put env value in starting of name
+#   runtime         = var.py_runtime
 # }
 
-
-# module "vpc_network" {
-#   source          = "../modules/vpc"
-#   cidr_block      = var.vpc_cidr_block
-#   vpc_tag         = var.vpc_tag
-#   public_subnet   = var.public_subnet_cidr_block
-#   private_subnet  = var.private_subnet_cidr_block
-#   env             = var.env
-#  }
-
-module "lambda_create"{
-  source          = "../modules/lambda"
-  env             = var.env
-  function_name   = var.env #Put env value in starting of name
-  runtime         = var.py_runtime
-}
-
-output "lambda_url"{
-  value = module.lambda_create.lambda_function_invoke_url
-}
+# output "lambda_url"{
+#   value = module.lambda_create.lambda_function_invoke_url
+# }
